61e0111c9b56976b517086b41bef828fac434a36
Pasta Mista <m.dambrosio21@gmail.com>
2017-03-15 11:31:54 +0000

    Added avout view,route, controller action

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
   def home    #Seite im pages Ordner muss genau diesen Namen beinhalten
   end
+  
+  def about
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 #routes wird bei Seitenaufruf zuerst aufgerufen, dann der pages_Controller, der die View zurück gibt
 Rails.application.routes.draw do
  root to: "pages#home"
+ get "about", to: "pages#about"  #über get Übermittlung in Adresszeile gelangt man zur About page
 end
