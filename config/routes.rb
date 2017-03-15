e53d9544f622b65b782fe52e30dd39548373907a
Pasta Mista <m.dambrosio21@gmail.com>
2017-03-15 11:33:54 +0000

    Added avout view,route, controller action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 #routes wird bei Seitenaufruf zuerst aufgerufen, dann der pages_Controller, der die View zurück gibt
 Rails.application.routes.draw do
  root to: "pages#home"
- get "about", to: "pages#about"  #über get Übermittlung in Adresszeile gelangt man zur About page
+ get "about", to: "pages#about"  #über get request in Adresszeile (/about) gelangt man zur About page
 end
