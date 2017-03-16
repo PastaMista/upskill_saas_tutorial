86f62b84520f8f947994a3b67e4ae31bfd03c816
Pasta Mista <m.dambrosio21@gmail.com>
2017-03-16 10:52:27 +0000

    Updated About Page and Logo Brand

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-#routes wird bei Seitenaufruf zuerst aufgerufen, dann der pages_Controller, der die View zurück gibt
+#routes wird bei Seitenaufruf zuerst aufgerufen, dann der pages_Controller, der die View zurück gibt (oackt sich das HTML und setzt es im Layout bei <%= yield %) ein, Layout gilt für alle Seiten
 Rails.application.routes.draw do
  root to: "pages#home"
  get "about", to: "pages#about"  #über get request in Adresszeile (/about) gelangt man zur About page
