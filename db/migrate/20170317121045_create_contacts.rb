52bc91b14996affb54278f04c824e47ef76d45f9
Pasta Mista <m.dambrosio21@gmail.com>
2017-03-17 12:25:35 +0000

    Added contact model file

--- db/migrate/20170317121045_create_contacts.rb
+++ db/migrate/20170317121045_create_contacts.rb
@@ -1,4 +1,4 @@
-class CreateContacts < ActiveRecord::Migration[5.0]
+class CreateContacts < ActiveRecord::Migration[5.0] #Migration sind wie Blueprints für einee DB. Immer wenn man die DB Struktur ändern möchte, nutzt man eine Migration Datei. Mit rails db:migrate wird BP angewendet 
   def change
     create_table :contacts do |t|
       t.string :name
