da2c71539e09593d56266a4cba07c1bd7e353c13
Pasta Mista <m.dambrosio21@gmail.com>
2017-03-19 12:35:00 +0000

    Form Valid

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,9 @@
-class Contact < ActiveRecord::Base  #checkt die DB, die Contact Tabelle und übernimmt die Attribute - müssen hier nicht extra nochmal eingetragen werden
-                                    #Model ist die Blueprint für eine Klasse
-end  
+#checkt die DB, die Contact Tabelle und übernimmt die Attribute (name, email, kommentar, timestamp) von db->shema.rb - müssen hier nicht extra nochmal eingetragen werden
+#Model ist die Blueprint für eine Klasse
+ # =>hier kommen spezifische Anforderungen an die Daten, wie Name nur 200 Zeichen
+ 
+class Contact < ActiveRecord::Base                
+   validates :name, presence: true
+   validates :email, presence: true  
+   validates :kommentar, presence: true
+end                                             
