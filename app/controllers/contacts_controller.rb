9ee6b0cc04150a0b618cdec705c622291274051f
Pasta Mista <m.dambrosio21@gmail.com>
2017-03-19 12:06:15 +0000

    added action form to save in databse

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,11 +1,12 @@
 class ContactsController < ApplicationController
+  
   def new  #schaut in views nach einer "new" Datei
-    @contact = Contact.new    # "@" = Instanz Variable
+    @contact = Contact.new    # "@" = Instanz Variable. Immer wenn contact/new aufgerufen wird, wird ein neues Objekt erstellt (hier leeres Objekt mit name: , email: , kommentar: )
   end
   
-  def create
-    @contact = Contact.new(contact_params)
-    if @contact.save
+  def create    #deafult Funktion, um Sachen in DB zu speichern
+    @contact = Contact.new(contact_params)   #{name: "Bob", email:"das@a.de", kommentar: "aksdkm"}
+    if @contact.save  #wenn Speicherung erfolgreich
       redirect_to new_contact_path, notice: "Kommentar gesendet"
     else
       redirect_to new_contact_path, notice: "Error K4pp4"
@@ -14,6 +15,6 @@ class ContactsController < ApplicationController
   
   private
     def contact_params
-      params.require(:contact).permit(:name, :email, :kommentar)
+      params.require(:contact).permit(:name, :email, :kommentar)   #rails strong parameters 
     end
 end
