9fa4602f8e0e11b0dc35b38e62e1aa0f55ef02bf
Pasta Mista <m.dambrosio21@gmail.com>
2017-03-24 09:51:24 +0000

    Added Bootstrap to signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 #routes wird bei Seitenaufruf zuerst aufgerufen, dann der pages_Controller, der die View zurück gibt (oackt sich das HTML und setzt es im Layout bei <%= yield %) ein, Layout gilt für alle Seiten
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users                             #devise erstellt einige routs, nice
  root to: "pages#home"
  get "about", to: "pages#about"               #über get request in Adresszeile (/about) gelangt man zur About page, wenn get "about" wird pages controller aufgerufen und auf about geleitet
  resources :contacts, only: :create                          #shortcut für: get "contacts/new", to "contacts#new"   erstellt einige Routen "rails routes" im Terminal. In diesem Fall wird nur vreate benötigt
