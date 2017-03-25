02ea474131ddef5962f8e9944a0902cea07c260f
Pasta Mista <m.dambrosio21@gmail.com>
2017-03-25 12:14:43 +0000

    Added routes for user profiles

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 #routes wird bei Seitenaufruf zuerst aufgerufen, dann der pages_Controller, der die View zurück gibt (oackt sich das HTML und setzt es im Layout bei <%= yield %) ein, Layout gilt für alle Seiten
 Rails.application.routes.draw do
   devise_for :users                             #devise erstellt einige routs, nice
- root to: "pages#home"
- get "about", to: "pages#about"               #über get request in Adresszeile (/about) gelangt man zur About page, wenn get "about" wird pages controller aufgerufen und auf about geleitet
- resources :contacts, only: :create                          #shortcut für: get "contacts/new", to "contacts#new"   erstellt einige Routen "rails routes" im Terminal. In diesem Fall wird nur vreate benötigt
- get "contact-us", to:"contacts#new", as: "new_contact"          #custom url für die Contacts Seite bei Get Anfrage auf spezielle Seite weiterleiten. Mit as: Muss new_contact_path in application.html layout nicht geänert werden
+  root to: "pages#home"
+
+  resources :users do
+    resource :profile     #has_one Relationship, deswegen singular
+  end
+  
+  get "about", to: "pages#about"               #über get request in Adresszeile (/about) gelangt man zur About page, wenn get "about" wird pages controller aufgerufen und auf about geleitet
+  resources :contacts, only: :create                          #shortcut für: get "contacts/new", to "contacts#new"   erstellt einige Routen "rails routes" im Terminal. In diesem Fall wird nur vreate benötigt
+  get "contact-us", to:"contacts#new", as: "new_contact"          #custom url für die Contacts Seite bei Get Anfrage auf spezielle Seite weiterleiten. Mit as: Muss new_contact_path in application.html layout nicht geänert werden
 end
