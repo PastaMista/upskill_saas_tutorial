d426f95f244fea496071645c19e2a4b03b9820dc
Pasta Mista <m.dambrosio21@gmail.com>
2017-03-25 21:02:28 +0000

    Added safe function to controller for Profiles

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,26 @@
 class ProfilesController < ApplicationController
     # GET to /users/:user_id/profile/new
     def new
-      @profile = Profile.new    #wenn Seite aufgerufen wird, wird eine leere Liste für ein Profil erstellt
+      @profile = Profile.new    #wenn Seite aufgerufen wird, wird eine leere Liste für ein Profil erstellt. Durch Profile(groß geschrieben) verweist man auf das Model
     end
+    
+    #Post request -> create
+    def create
+      #User wird ermittelt
+      @user = User.find(params[:user_id])
+      #Profile wird erstellt und mit User verknüpft
+      @profile = @user.build_profile( profile_params )
+      if @profile.save
+        flash[:success] = "Profil geuuuuuupdatet"
+        redirect_to root_path
+      else
+        render action: :new
+      end
+    end
+    
+    #whitelist the params
+    private
+      def profile_params
+        params.require(:profile).permit(:first_name, :last_name, :job, :phone_number, :contact_email, :description)
+      end
 end
